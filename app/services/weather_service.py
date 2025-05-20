b612bf06507f5c4c80965f5c6961ed6d26136568
Okonkwo Kamso <okonkwochikamsoj@gmail.com>
2025-05-20 22:25:10 +0100

    Added params and headers. handled timeout and connection errors

--- app/services/weather_service.py
+++ app/services/weather_service.py
@@ -1,5 +1,6 @@
 import httpx
 import os
+from fastapi import HTTPException
 from dotenv import load_dotenv
 import json
 from upstash_redis import Redis
@@ -8,28 +9,57 @@ from typing import Any, Dict
 load_dotenv()
 
 API_KEY: str = os.getenv("TOMORROW_API_KEY", "")
+CACHE_TTL = 3600
 print(f"API_KEY: {API_KEY}")
 
 async def get_weather(city: str, redis: Redis) -> Dict[str, Any]:
+
   cache_key = f"weather:{city.lower()}"
 
   cached_data: str | None = redis.get(cache_key)
 
   if cached_data:
-    return json.loads(cached_data)
+    return httpx.Response(200, content=cached_data).json()
+  
+  params = {
+    "location": city,
+    "units": "metric"
+  }
+
+  headers = {
+    "accept": "application/json",
+    "apikey": API_KEY
+  }
   
   url = (
     f"https://api.tomorrow.io/v4/weather/realtime"
     f"?location={city}&apikey={API_KEY}"
   )
 
-  async with httpx.AsyncClient() as client:
-    response = await client.get(url)
-    response.raise_for_status()
-    weather_data: Dict[str, Any] =  response.json()
+  try:
+    async with httpx.AsyncClient() as client:
+      response = await client.get(url, params=params, headers=headers)
+      response.raise_for_status()
+      weather_data: Dict[str, Any] =  response.json()
 
-  redis.set(cache_key, json.dumps(weather_data), ex=600)
+      redis.set(cache_key, json.dumps(weather_data), ex=CACHE_TTL)
 
-  print(f"Weather data for {city}:", weather_data)
+      print(f"Weather data for {city}:", weather_data)
+
+      return weather_data
+
+  except httpx.RequestError as exc:
+    print(f'Request error: {exc}')
+    raise HTTPException(status_code=503, detail="Service unavailable")
+  
+  except httpx.HTTPStatusError as exc:
+    print(f'API error: {exc.response.status_code} - {exc.response.text}')
+    raise HTTPException(status_code=exc.response.status_code, detail="Error fetching weather data")
+
+
+
+
+
+
 
   return weather_data
