6c9f6fb5e77ac3fb54243cd001d160907cde9ccf
Okonkwo Kamso <okonkwochikamsoj@gmail.com>
2025-05-20 03:08:56 +0100

    changed redis config to uptash redis

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -1,11 +1,16 @@
-from redis.asyncio import Redis 
+#from redis.asyncio import Redis 
+from upstash_redis import Redis
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIS_URL: str = os.getenv("REDIS_URL")
+redis_url: str = os.getenv("REDIS_URL")
+redis_token: str = os.getenv("REDIS_TOKEN")
+print(f"REDIS_TOKEN: {redis_token}")
 
-print(f"REDIS_URL: {REDIS_URL}")
+print(f"REDIS_URL: {redis_url}")
 
-redis_client: Redis = Redis.from_url(REDIS_URL, decode_responses=True)
+# redis_client: Redis = Redis.from_url(REDIS_URL, decode_responses=True)
+
+redis = Redis(url="redis_url", token="redis_token")
