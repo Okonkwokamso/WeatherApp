874fbee2d20040a0dfdba71995542ec506592ba0
Okonkwo Kamso <okonkwochikamsoj@gmail.com>
2025-05-23 21:19:34 +0100

    Added a hello message to the root endpoint

--- app/main.py
+++ app/main.py
@@ -8,23 +8,29 @@ app = FastAPI(title="Weather API")
 # def get_redis_client() -> Redis:
 #   return redis_client
 
+@app.get("/")
+async def root():
+  return {
+    "message": "Welcome to your weather buddy",
+    "status": "success",
+    "documentation": "Visit /docs for API documentation."
+  }
+
 @app.get("/weather/")
 async def weather(location: str = Query(..., min_length=2)):
 
   try:
     weather_data = await get_weather(location, redis_client)
 
+    return weather_data
+
     # return {
-    #   "location": city,
-    #   "data": weather_data.get("data", {})
+    #   "location": location,
+    #   "temperature": weather_data["data"]["values"]["temperature"],
+    #   "humidity": weather_data["data"]["values"]["humidity"],
+    #   "wind_speed": weather_data["data"]["values"]["windSpeed"],
+    #   "rainIntensity": weather_data["data"]["values"]["rainIntensity"],
     # }
-    return {
-      "location": location,
-      "temperature": weather_data["data"]["values"]["temperature"],
-      "humidity": weather_data["data"]["values"]["humidity"],
-      "wind_speed": weather_data["data"]["values"]["windSpeed"],
-      "rainIntensity": weather_data["data"]["values"]["rainIntensity"],
-    }
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
                     
